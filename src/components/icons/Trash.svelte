81d408621be08117c0947bed140d14c60d94bf98
Corro kun <networpava@gmail.com>
2024-06-21 11:26:32 -0500

    add icons

---
+++ src/components/icons/Trash.svelte
@@ -0,0 +1,21 @@
+<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path class="body" d="M4 7l16 0" />
+    <path class="body" d="M10 11l0 6" />
+    <path class="body" d="M14 11l0 6" />
+    <path class="body" d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+    <path class="body" d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+</svg>
+
+<style>
+    svg{
+        cursor: pointer;
+    }
+    .body{
+        transition: .2s;
+        stroke: var(--Color_Primary);
+    }
+    svg:hover .body{
+        stroke: var(--Color_Secondary);
+    }
+</style>
