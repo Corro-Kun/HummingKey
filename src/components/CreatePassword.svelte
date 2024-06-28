355b1efc16cf5b069c81344d0c8a40256decb240
Corro kun <networpava@gmail.com>
2024-06-27 23:16:38 -0500

    init CreatePassword

---
+++ src/components/CreatePassword.svelte
@@ -0,0 +1,14 @@
+<form>
+    <h2>form</h2>
+</form>
+
+<style>
+    form{
+        height: 500px;
+        width: 500px;
+        background: transparent;
+        backdrop-filter: blur(10px);
+        border: 2px solid var(--Color_Primary);
+        border-radius: 8px;
+    }
+</style>
